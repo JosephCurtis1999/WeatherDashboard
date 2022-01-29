a3e3d17879be5fb07ff6d335eab3dd7cff08f278
Joe Curtis <joecurtis9999@gmail.com>
2022-01-29 18:41:31 +0000

    added openweather api variables, without api variables and key the collection of data for the dashboard will not work

---
+++ Assets/js/script.js
@@ -0,0 +1,7 @@
+var openWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q="
+var openWeatherOneCallAPI = "https://api.openweathermap.org/data/2.5/onecall?"
+var excludeString = "&exclude=minutely,hourly,alerts"
+var weatherUnits = "&units=imperial"
+var apiKey = "&appid=47cc7111aeaa92ded720903e4f89338c"
+var iconURL = "https://openweathermap.org/img/wn/"
+
