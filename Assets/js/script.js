1962365360a06a714431158190ec799b621041e1
Joe Curtis <joecurtis9999@gmail.com>
2022-01-29 18:43:44 +0000

    added an empty last search and last search default string so that the search bar is empty when a user goes to find a city

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,4 +5,7 @@ var weatherUnits = "&units=imperial"
 var apiKey = "&appid=47cc7111aeaa92ded720903e4f89338c"
 var iconURL = "https://openweathermap.org/img/wn/"
 var searchCityList = [];
+var lastSearch = '';
+var lastSearchDefault = '';
+var cityNameOK = true;
 
