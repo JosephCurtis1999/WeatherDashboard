7f86e5ed757eb7e9981dd3f1feacdec43e2c44ec
Joe Curtis <joecurtis9999@gmail.com>
2022-02-04 18:18:15 +0000

    added a save search variable to save the users searches

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,5 @@
 var apiKey ="742e1e6ab34a390c2330f87279037795";
 
+// using JSON parse we can covert data into string form into JS objects, get item gets the stored items
+var savedSearch = JSON.parse(localStorage.getItem("ForecastHistory")) || [];
+
