a225dceb83ad7851a524f10a0316536cfe77ea03
Joe Curtis <joecurtis9999@gmail.com>
2022-02-04 18:23:00 +0000

    added a for loop for the saved search

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,3 +3,9 @@ var apiKey ="742e1e6ab34a390c2330f87279037795";
 // using JSON parse we can covert data into string form into JS objects, get item gets the stored items
 var savedSearch = JSON.parse(localStorage.getItem("ForecastHistory")) || [];
 
+// creating a for loop
+for (i = 0; i < savedSearch.length; i++) {
+    var cityBtn = $(
+    '<button class="list-group-item" data-city="${savedSearch[i]}">${savedSearch[i]}</button>'
+    );
+}
